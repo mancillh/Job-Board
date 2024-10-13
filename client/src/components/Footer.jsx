762095d005625f084c2405d3dcd381e26c50cdea
LeonathaTurtle <mancillh@gmail.com>
2024-10-13 12:32:52 -0500

    changed component files to .jsx, uninstalled react-scripts, changed scripts, react functional, basic header and footer added

---
+++ client/src/components/Footer.jsx
@@ -0,0 +1,5 @@
+function Footer() {
+    return <h5>Made with love by Kent Ball, Thomas Leavitt, Tracey Litschgi, Hannah Mancill, & Joshua Nanninga</h5>;
+  }
+
+  export default Footer;
---
+++ client/src/components/Header.jsx
@@ -0,0 +1,12 @@
+import {Container} from './styles/Container.styled.js';
+
+function Header() {
+   
+    return <>
+      <Container>
+        <h1>NeighborhoodJobs</h1>
+      </Container>
+    </>;
+  }
+
+  export default Header;
