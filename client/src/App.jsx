c5aeb9121e4220d5366184861c3eaebfa625ce39
LeonathaTurtle <mancillh@gmail.com>
2024-10-15 18:57:05 -0500

    navbar and header created with semantic and css, styles folder for css files created, corkboard header banner, and sticky note icon added to public

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,11 +1,13 @@
+import 'semantic-ui-css/semantic.min.css'
+import Navbar from './components/Navbar';
 import Header from './components/Header';
 import Footer from './components/Footer';
-
 
 function App() {
 
   return (
     <>
+      <Navbar />
       <Header />
       <Footer />
     </>
--- client/src/components/Header.jsx
+++ client/src/components/Header.jsx
@@ -1,12 +1,14 @@
-import {Container} from './styles/Container.styled.js';
+import { Image } from 'semantic-ui-react'
+import '../styles/Header.css';
 
 function Header() {
-   
-    return <>
-      <Container>
-        <h1>NeighborhoodJobs</h1>
-      </Container>
-    </>;
-  }
 
-  export default Header;
+  return <>
+    <div className='header-container'>
+      <Image className='sticky-icon' src='/sticky note with push pin icon.png' size='small' />
+      <h1>Neighborhood Jobs</h1>
+    </div>
+  </>;
+}
+
+export default Header;
