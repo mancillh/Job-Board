0edca568e168801e971675c56394c039210d63c3
Joshua Nanninga <jmarknanninga@gmail.com>
2024-10-14 19:50:39 -0500

    put in app.jsx

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,15 +1,21 @@
+import React from 'react';
 import Header from './components/Header';
 import Footer from './components/Footer';
-
+import Corkboard from './components/Corkboard'; // Assuming you have a Corkboard component
 
 function App() {
-
   return (
     <>
       <Header />
+      
+      {/* Main content section */}
+      <main>
+        <Corkboard />  {/* Main interactive job listing corkboard */}
+      </main>
+      
       <Footer />
     </>
-  )
+  );
 }
 
-export default App
+export default App;
