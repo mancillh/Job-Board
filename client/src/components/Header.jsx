bc589b5e2e3636d8825ebfc962789fd8c807c952
Kent Ball <kent.vincent.ball@gmail.com>
2024-10-18 18:59:08 -0400

    Added search bar to Header page + styling

--- client/src/components/Header.jsx
+++ client/src/components/Header.jsx
@@ -1,15 +1,29 @@
-import { Image } from 'semantic-ui-react'
+import React from 'react';
+import { Image, Input } from 'semantic-ui-react';
 import '../styles/Header.css';
 
 function Header() {
-
-  return <>
-    <div className='header-container'>
-      <Image className='sticky-icon' src='/sticky note with push pin icon.png' size='small' />
-      <h1>Neighborhood Jobs</h1>
-    </div>
-    <div className="horizontal-line"></div>
-  </>;
+  return (
+    <>
+      <div className='header-container'>
+        <Image className='sticky-icon' src='/sticky note with push pin icon.png' size='small' />
+        <div className='header-content'>
+          <h1>Neighborhood Jobs</h1>
+          <div className='search-container'>
+            <Input
+              fluid
+              icon='search'
+              iconPosition='left'
+              placeholder='Search for jobs, skills, or companies...'
+              size='huge'
+              className='job-search-bar'
+            />
+          </div>
+        </div>
+      </div>
+      <div className="horizontal-line"></div>
+    </>
+  );
 }
 
 export default Header;
