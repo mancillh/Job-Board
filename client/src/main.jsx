2d3be21efa92d7de14d3bcc6d6e7cb5b46687009
Kent Ball <kent.vincent.ball@gmail.com>
2024-10-20 12:40:09 -0400

    signup/logout/login functionality

--- client/src/main.jsx
+++ client/src/main.jsx
@@ -1,13 +1,44 @@
 import ReactDOM from 'react-dom/client'
 import { createBrowserRouter, RouterProvider } from 'react-router-dom'
+import { ApolloClient, InMemoryCache, ApolloProvider, createHttpLink } from '@apollo/client';
+import { setContext } from '@apollo/client/link/context';
+import { ApolloLink } from '@apollo/client';
 
 import App from './App.jsx';
-// import Signup from './pages/Signup';
-// import Login from './pages/Login';
+import Signup from './pages/Signup';
+import Login from './pages/Login';
 import Error from './pages/Error';
 import About from './pages/About';
 import Home from './pages/Home';
 import Contact from './pages/Contact';
+
+const loggerLink = new ApolloLink((operation, forward) => {
+  console.log(`GraphQL Request: ${operation.operationName}`);
+  return forward(operation).map((result) => {
+    console.log(`GraphQL Result: ${operation.operationName}`, result);
+    return result;
+  });
+});
+
+// Set up Apollo Client
+const httpLink = createHttpLink({
+  uri: 'http://localhost:3001/graphql', // Explicitly specify the full URL
+});
+
+const authLink = setContext((_, { headers }) => {
+  const token = localStorage.getItem('id_token');
+  return {
+    headers: {
+      ...headers,
+      authorization: token ? `Bearer ${token}` : '',
+    }
+  };
+});
+
+const client = new ApolloClient({
+  link: authLink.concat(httpLink),
+  cache: new InMemoryCache()
+});
 
 const router = createBrowserRouter([
   {
@@ -17,23 +48,25 @@ const router = createBrowserRouter([
     children: [
       {
         index: true,
-        path: '/',
         element: <Home />
-      }, 
-      // {
-      //   path: '/login',
-      //   element: <Login />
-      // }, {
-      //   path: '/signup',
-      //   element: <Signup />
-      // }, 
+      },
+      {
+        path: '/login',
+        element: <Login />
+      },
+      {
+        path: '/signup',
+        element: <Signup />
+      },
       {
         path: '/home',
         element: <Home />
-      },{
+      },
+      {
         path: '/about',
         element: <About />
-      },{
+      },
+      {
         path: '/contact',
         element: <Contact />
       }
@@ -42,5 +75,7 @@ const router = createBrowserRouter([
 ])
 
 ReactDOM.createRoot(document.getElementById('root')).render(
-  <RouterProvider router={router} />
+  <ApolloProvider client={client}>
+    <RouterProvider router={router} />
+  </ApolloProvider>
 )
---
+++ client/src/pages/Login.jsx
@@ -0,0 +1,73 @@
+import React, { useState } from 'react';
+import { Link } from 'react-router-dom';
+import { useMutation } from '@apollo/client';
+import { LOGIN_USER } from '../utils/mutations';
+import Auth from '../utils/auth';
+import { Form, Button, Container, Header, Message } from 'semantic-ui-react';
+import '../styles/AuthPages.css';
+
+const Login = () => {
+  const [formState, setFormState] = useState({ email: '', password: '' });
+  const [login, { error }] = useMutation(LOGIN_USER);
+
+  const handleChange = (event) => {
+    const { name, value } = event.target;
+    setFormState({
+      ...formState,
+      [name]: value,
+    });
+  };
+
+  const handleFormSubmit = async (event) => {
+    event.preventDefault();
+    try {
+      const { data } = await login({
+        variables: { ...formState },
+      });
+      Auth.login(data.login.token);
+    } catch (e) {
+      console.error(e);
+    }
+  };
+
+  return (
+    <div className="auth-page">
+      <Container className="auth-container">
+        <Header as='h2' textAlign='center' className="auth-header">Login</Header>
+        <Form onSubmit={handleFormSubmit} className="auth-form">
+          <Form.Input
+            fluid
+            icon='mail'
+            iconPosition='left'
+            placeholder='Your email'
+            name="email"
+            type="email"
+            value={formState.email}
+            onChange={handleChange}
+          />
+          <Form.Input
+            fluid
+            icon='lock'
+            iconPosition='left'
+            placeholder='Password'
+            name="password"
+            type="password"
+            value={formState.password}
+            onChange={handleChange}
+          />
+          <Button color='black' fluid size='large' type='submit'>
+            Login
+          </Button>
+        </Form>
+        {error && (
+          <Message negative className="auth-message">
+            <Message.Header>Error</Message.Header>
+            <p>{error.message}</p>
+          </Message>
+        )}
+      </Container>
+    </div>
+  );
+};
+
+export default Login;
