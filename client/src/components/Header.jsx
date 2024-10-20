7e387b052fb493c4ea0262065748ca6341b834ed
Joshua Nanninga <jmarknanninga@gmail.com>
2024-10-20 15:50:10 -0500

    added useState and Input, along with search box integration

--- client/src/components/Header.jsx
+++ client/src/components/Header.jsx
@@ -1,12 +1,29 @@
-import { Image } from 'semantic-ui-react'
+import React, { useState } from 'react';
+import { Image, Input } from 'semantic-ui-react'
 import '../styles/Header.css';
 
 function Header() {
+  const [searchQuery, setSearchQuery] = useState('');
+
+  const handleSearchChange = (e) => {
+    setSearchQuery(e.target.value);
+    // You can implement additional search logic here
+    console.log('Search Query:', e.target.value);
+  };
 
   return <>
     <div className='header-container'>
       <Image className='sticky-icon' src='/sticky note with push pin icon.png' size='small' />
       <h1>Neighborhood Jobs</h1>
+      {/* Search Box Integration */}
+      <div className='search-box'>
+          <Input
+            icon='search'
+            placeholder='Search...'
+            value={searchQuery}
+            onChange={handleSearchChange}
+          />
+        </div>
     </div>
     <div className="horizontal-line"></div>
   </>;
