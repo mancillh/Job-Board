82dd8b7e93d51fa3f3cd8202023396de1283e450
Joshua Nanninga <jmarknanninga@gmail.com>
2024-10-22 12:01:13 -0500

    added useEffect for handling fetching jobs

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -3,8 +3,8 @@ import { Outlet } from 'react-router-dom';
 import Navbar from './components/Navbar';
 import Header from './components/Header';
 import Footer from './components/Footer';
-import { useState } from 'react';
-import { query } from 'express';
+import { useState, useEffect } from 'react';
+// import { query } from 'express';
 
 function App() {
   const [searchQuery, setSearchQuery] = useState('');
