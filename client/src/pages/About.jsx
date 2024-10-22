a9456df579da6ff011bf6b0148b6366c0be75431
Joshua Nanninga <jmarknanninga@gmail.com>
2024-10-21 19:52:55 -0500

    added about us

--- client/src/pages/About.jsx
+++ client/src/pages/About.jsx
@@ -5,7 +5,7 @@ function About() {
     <div className='cork-container'>
       <div className='content-wrapper'>
         <h2>About Us</h2>
-        <p>This is some text about us!</p>
+        <p>The Neighborhood Job Board connects employers with qualified candidates for successful employment matches. This platform offers job seekers an easy-to-use space to search, view, and apply for a wide range of job opportunities. Additionally, users can create personalized profiles to save their favorite job postings and upload their resumes, streamlining their job search process.</p>
       </div>
     </div>
   );
