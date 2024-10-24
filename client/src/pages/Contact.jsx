97f2c7a70ca927e58f55e4065574805affbb0031
tleavitt25526 <tleavitt25526@gmail.com>
2024-10-24 19:59:58 -0400

    Contact and About Us finished

--- client/src/pages/Contact.jsx
+++ client/src/pages/Contact.jsx
@@ -5,11 +5,14 @@ function Contact() {
     <div className='cork-container'>
       <div className='content-wrapper'>
         <h2>Contact Us</h2>
-        <p>Phone: 123.456.7890</p>
-        <p>Email: contact@neighborhoodjobs.com</p>
+        <p>Hannah Mancil: mancillh@gmail.com</p>
+        <p>Joshua Nanninga: example@gmail.com</p>
+        <p>Kent Ball: example@gmail.com</p>
+        <p>Thomas Leavitt: tleavitt25526@gmail.com</p>
+        <p>Tracey Litschgi: example@gmail.com</p>
       </div>
     </div>
   );
 }
 
-export default Contact;
+export default Contact; 
